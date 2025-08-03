1e6ff4a84ff3fb5a0f2f84988accaeec9321e7db
Zzzrbn <zzzrbn@yandex.ru>
2025-08-03 14:27:26 +0300

    Initial commit

---
+++ src/main/java/com/zzzrbn/task_api_gateway/TaskApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zzzrbn.task_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApiGatewayApplication.class, args);
+	}
+
+}
